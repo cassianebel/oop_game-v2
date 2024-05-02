5627d6f372728f9bf662378ae69584e6b44dbb0b
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-05-01 20:30:49 -0500

    addPhraseToDisplay

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,25 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+
+  /**
+  * Display phrase on game board
+  */
+  addPhraseToDisplay() {
+    const phraseDiv = document.getElementById('phrase');
+    const ul = phraseDiv.querySelector('ul');
+    const characters = this.phrase.split('');
+    let html = ``;
+  
+    for(let i = 0; i < characters.length; i++) {
+      if(characters[i] === ' ') {
+        html += `<li class="space"> </li>`
+      } else {
+        html += `<li class="hide letter ${characters[i]}">${characters[i]}</li>`;
+      }
+    }
+
+    ul.innerHTML = html;
+
+  };
 }
