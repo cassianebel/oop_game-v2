89e4301e4ecacfb2faf28fe94bb5b6ab09aed9c0
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-05-01 20:12:06 -0500

    added phrases and getRandomPhrase method

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [new Phrase('A DIAMOND IN THE ROUGH'), 
+                    new Phrase('A WALK IN THE THE PARK'), 
+                    new Phrase('BETTER LATE THAN NEVER'),  
+                    new Phrase('BETTER SAFE THAN SORRY'),  
+                    new Phrase('GET ALL YOUR DUCKS IN A ROW'),  ];
+    this.activePhrase = null;
+  }
+
+  /**
+  * Selects random phrase from phrases property
+  * @return {Object} Phrase object chosen to be used
+  */
+  getRandomPhrase() {
+    let num = Math.floor(Math.random() * this.phrases.length);
+    console.log(num);
+    return this.phrases[num];
+  };
+}
